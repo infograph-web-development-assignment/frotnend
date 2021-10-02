78b4121e966bb69db86d6f5e832e6f776d009b19
FIRAS AHMAD HASAN <x.firashasan@gmail.com>
2021-10-02 03:25:52 +0300

    rendering data from API

--- frontend-infograph/src/App.js
+++ frontend-infograph/src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Main from './components/Main';
 
 import Header from './components/Header';
 export default function App() {
   return (
+    <>
+    <div>
   <Header/>
+  </div>
+  <div>
+  <Main/>
+  </div>
+  </>
   );
 }
 
