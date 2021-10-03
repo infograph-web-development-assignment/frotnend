cfed252a695dce660e45acbf2ca93f3e1fd3ad4b
FIRAS AHMAD HASAN <x.firashasan@gmail.com>
2021-10-03 05:51:00 +0300

    strict the user to create an account to enter website

--- frontend-infograph/src/App.js
+++ frontend-infograph/src/App.js
@@ -2,15 +2,24 @@
 
 import React from "react";
 import Register from "./components/Register";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Login from "./components/Login";
 
 class App extends React.Component {
-  
-
   render() {
     return (
       <>
-      <Register/>
-        </>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/'>
+              <Register />
+            </Route>
+            <Route exact path='/login'>
+              <Login />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </>
     );
   }
 }
